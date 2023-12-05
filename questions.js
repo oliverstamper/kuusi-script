35cfcb10cc304b33313bbabf25a68f6e8e5490e6
ollystamper <148012712+oliverstamper@users.noreply.github.com>
2023-12-05 09:10:33 +0000

    questions added

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: 'Commonly used data types DO NOT include ______',
+        options: ['strings','booleans','alerts','numbers'],
+        correctAnswer: 'alerts'
+    },
+    {
+        question:'The condition in an if/else statement is enclosed within ______.',
+        options: ['quotes', 'curly brackets', 'parenthesis', 'square braackets'],
+        correctAnswer: 'parenthesis'
+    }
+    {
+      question: 'Arrays in JavaScript can be used to store _______.',
+      options: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+      correctAnswer: 'all of the above'
+    },
+    {
+      question: 'String values must be enclosed within ______ when being assigned to varirables.',
+      options: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+      correctAnswer: 'quotes'
+    },
+    {
+      question: 'A very useful tool during development and debugging for printing content to the debugger is:',
+      options: ['JavaScript', 'terminal/hash', 'for loops', 'console log'],
+      correctAnswer: 'console log'
+    },
+  ];
